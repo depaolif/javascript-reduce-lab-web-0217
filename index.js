5ea203a5e5a640c48ac796f73732ced5b78c0dd1
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-17 16:55:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+var totalBatteries = batteryBatches.reduce((a,b) => {return a+b}, 0)
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +18,14 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var wordCountMap = monologueLines.reduce((acc,val) => {
+  let len = val.split(' ').length
+  if (acc[len]) {
+    acc[len]++
+    return acc
+  } else {
+    acc[len] = 1
+    return acc
+  }
+}, {})
